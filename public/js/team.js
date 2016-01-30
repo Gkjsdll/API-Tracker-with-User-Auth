0b05d42290975774f291973dc68890b817b54c23
Zack Winchell <zackwinchell@gmail.com>
2016-01-30 00:55:34 -0800

    added pokemon images and species to team view

--- public/js/team.js
+++ public/js/team.js
@@ -1,11 +1,27 @@
 "use strict";
 
-$.get("/users/profile")
-.success(function(data){
-  console.log(data.pokemon);
-  $("#loading").hide();
-  for(var pokemonIndex in data.pokemon){
-    console.log(`pokemon${pokemonIndex}`);
-    console.log(data.pokemon[pokemonIndex]);
-  }
+$(document).ready(function(){
+  $.get("/users/profile")
+  .success(function(data){
+    $("#loading").hide();
+    for(var pokemonIndex in data.pokemon){
+      getPokeData(data.pokemon[pokemonIndex].poketype);
+    }
+    $("#teamContainer").show();
+  })
 })
+
+function getPokeData(which){
+  $.get(`http://pokeapi.co/api/v1/pokemon/${which}`)
+  .success(function(pokedata){
+    var $pokemon = $("<div>").addClass("pokemon col-xs-2")
+    $pokemon.append($("<p>").text(pokedata.name));
+    $pokemon.append($("<button>").addClass("btn btn-default").text("Release Pokémon"));
+    $pokemon.data("apidata", pokedata)
+    $.get(`http://www.pokeapi.co${pokedata.sprites[0].resource_uri}`)
+    .success(function(data){
+      $pokemon.prepend($("<img>").attr("src", `http://www.pokeapi.co${data.image}`));
+      $("#team").append($pokemon);
+    })
+  });
+}
